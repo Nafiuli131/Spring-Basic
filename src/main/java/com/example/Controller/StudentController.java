05d79c4ecccb4d964aa5e0179b1c90b80004cefe
Nafiul Islam <nafiul.islam@synesisit.info>
2022-06-25 18:45:11 +0600

    rest api which returns list of objects

--- src/main/java/com/example/Controller/StudentController.java
+++ src/main/java/com/example/Controller/StudentController.java
@@ -4,10 +4,25 @@ import com.example.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
+//    return a bean class object
     @GetMapping(path = "/student")
     public Student getStudentInfo(){
         return new Student("Nafiul","Islam");
+    }
+
+    //return a list of object
+    @GetMapping("/students")
+    public List<Student> getStudentList(){
+        List<Student> addStudent = new ArrayList<>();
+        addStudent.add(new Student("john","cena"));
+        addStudent.add(new Student("tony","cruise"));
+        addStudent.add(new Student("kuber","majhi"));
+        addStudent.add(new Student("montu","mia"));
+        return addStudent;
     }
 }
